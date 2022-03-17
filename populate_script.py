866d481af289ee961c971af2f86b0c26cb7510bd
ahmedfaizandar <2677937d@student.gla.ac.uk>
2022-03-17 15:55:05 +0000

    Populate script

---
+++ populate_script.py
@@ -0,0 +1,30 @@
+# from EventsGow.models import Category, Page
+# from django.core.files import File
+# import os
+# import glob
+
+import os
+from events.models import Tags
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventsGow.settings')
+import django
+django.setup()
+
+tag_list = ["food", "outdoors", "indoors", "sports", "music", "film", "trekking", "whisky", "party"]
+
+for i in tag_list:
+    tag = Tags(tag_name = i)
+    tag.save()
+
+# tag = Tags(tag_name="outdoors")
+# tag = Tags(tag_name="indoors")
+# tag = Tags(tag_name="sports")
+# tag = Tags(tag_name="trekking")
+# tag = Tags(tag_name="food")
+# tag = Tags(tag_name="music")
+# tag = Tags(tag_name="film")
+
+# Start excution here!
+# if __name__ == '__main__':
+#     print('Starting EventsGow population script...')
+#     populate()
