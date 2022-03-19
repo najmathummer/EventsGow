e1513a2d7be939081b8bc0b89ed4d0544329dd8f
ahmedfaizandar <2677937d@student.gla.ac.uk>
2022-03-19 15:47:52 +0000

    Cleaning code in populate script

--- populate_script.py
+++ populate_script.py
@@ -7,19 +7,11 @@ django.setup()
 
 from events.models import Tags
 
-tag_list = ["food", "outdoors", "indoors", "sports", "music", "film", "trekking", "whisky", "party"]
+tag_list = ["food", "outdoors", "indoors", "sports", "music", "film", "trekking", "whisky", "party", "adventure"]
 
 for i in tag_list:
     tag = Tags(tag_name = i)
     tag.save()
-
-# tag = Tags(tag_name="outdoors")
-# tag = Tags(tag_name="indoors")
-# tag = Tags(tag_name="sports")
-# tag = Tags(tag_name="trekking")
-# tag = Tags(tag_name="food")
-# tag = Tags(tag_name="music")
-# tag = Tags(tag_name="film")
 
 # Start excution here!
 # if __name__ == '__main__':
