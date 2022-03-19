9f1d5ae7726ba58af5011d3c3c24f10f53b3cb23
Ciao-Wen-Chen <ogh478@gmail.com>
2022-03-19 17:43:28 +0000

    unit test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class LandingPageTests(TestCase):
+    
+    def test_Landing_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 302)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('events:home'))
+        print("===" + str(response) + "\n")
+        self.assertEquals(response.url, "/accounts/login/?next=/")
--- events/tests.py
+++ events/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-class LandingPageTests(TestCase):
-    
-    def test_Landing_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 302)
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('events:home'))
-        print("===" + str(response) + "\n")
-        self.assertEquals(response.url, "/accounts/login/?next=/")
